c7475e3c93919252773379fed3388c3a9b4dc63d
Andrii Yanchuk <horeca288@gmail.com>
2026-06-22 12:37:28 +0300

    feat: add keno game page

---
+++ src/app/(main)/keno/page.tsx
@@ -0,0 +1,5 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function KenoRedirectPage() {
+  permanentRedirect("/all-games/keno");
+}
---
+++ src/screens/keno/index.ts
@@ -0,0 +1,1 @@
+export { KenoScreen } from "./KenoScreen";
